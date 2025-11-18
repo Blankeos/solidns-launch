6a7457a0de75ed6acc7a1d748792ed4abb6cc8a5
Blankeos <carloantonioct@gmail.com>
2025-11-18 09:12:26 +0800

    feat: upgraded to ns 9.0.

---
+++ .migration_backup/webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require("@nativescript/webpack");
+require("dotenv").config();
+
+module.exports = (env) => {
+  webpack.init(env);
+
+  webpack.chainWebpack((config) => {
+    config.devServer.hotOnly(true);
+    config.devServer.hot(true);
+
+    // Polyfills:
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      url: require.resolve("url/"),
+      util: require.resolve("util/"), // Add this
+    };
+
+    config.plugin("DefinePlugin").tap((args) => {
+      Object.assign(args[0], {
+        __PUBLIC_API_URL__: JSON.stringify(process.env.PUBLIC_API_URL),
+      });
+      return args;
+    });
+  });
+
+  return webpack.resolveConfig();
+};
